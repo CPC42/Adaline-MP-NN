3611dee40cb1979e86d95d6ddcb7e52eafe613c7
CPC42 <78859590+CPC42@users.noreply.github.com>
2023-09-12 06:57:36 +0200

    First version importing the dataset

---
+++ main.py
@@ -0,0 +1,47 @@
+
+import pandas as pd
+class csvReader():
+
+    def __init__(self):
+        #Leer todos los ficheros csv como matrices
+        OGcsv=[]
+        OGcsv.append(pd.read_csv('Original_Data/gt_2011.csv'))
+        OGcsv.append(pd.read_csv('Original_Data/gt_2012.csv'))
+        OGcsv.append(pd.read_csv('Original_Data/gt_2013.csv'))
+        OGcsv.append(pd.read_csv('Original_Data/gt_2014.csv'))
+        OGcsv.append(pd.read_csv('Original_Data/gt_2015.csv'))
+
+        #Concatenar los datos
+        self.ogData = pd.concat([OGcsv[0], OGcsv[1], OGcsv[2], OGcsv[3], OGcsv[4]], axis=0, ignore_index=True)
+
+
+        # Convertir DataFrame a matriz de valores
+
+        print(self.ogData.min())
+    def _getMaxMin(self):
+        return self.ogData.max().values, self.ogData.min().values
+
+
+    def getNormalized(self):
+        normData = self.ogData.copy()
+
+        maxCol, minCol = self._getMaxMin()
+
+        for i in range(len(normData.axes[0])):
+            for j in range(len(normData.axes[1])):
+                normData.iat[i,j]=(normData.iat[i,j]-minCol[j])/(maxCol[j]-minCol[j])
+
+        return normData
+
+    def randomizeData(self):
+
+        return self.getNormalized().sample(frac=1, random_state=0, ignore_index=True)
+
+    def splitData(self):
+
+        trainingData, validationData = self.randomizeData().split()
+
+
+if __name__ == '__main__':
+    a= csvReader()
+    print(a.randomizeData())
